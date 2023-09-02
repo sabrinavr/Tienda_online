47b4d54a490877e2781aa91743cc9c19ccca7d85
sabrinavr <sabrinavr81@gmail.com>
2023-09-02 01:03:43 -0600

    Actualizado vercel

--- servicios/login.servicios.js
+++ servicios/login.servicios.js
@@ -1,10 +1,10 @@
 const listaLogin = () =>{
-    return fetch("http://localhost:3000/users")
+    return fetch("https://fake-api-rust.vercel.app/users")
     .then((respuesta) => respuesta.json());
 }
 
 const crearLogin = (email, password) => {
-    return fetch("http://localhost:3000/users", {
+    return fetch("https://fake-api-rust.vercel.app/users", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
--- servicios/productos-servicios.js
+++ servicios/productos-servicios.js
@@ -1,19 +1,19 @@
 //GET 
 
 const listaProductos = () => 
-    fetch("http://localhost:3000/productos")
+    fetch("https://fake-api-rust.vercel.app/productos")
     .then((respuesta) => respuesta.json())
     .catch((error) => console.log(error));
 
 const listarUnProducto = (id) => {
-    return fetch(`http://localhost:3000/productos/${id}`).then((respuesta) => {
+    return fetch(`https://fake-api-rust.vercel.app/productos/${id}`).then((respuesta) => {
         return respuesta.json();
     });
 };
 
 //POST
 const crearProducto = (imageUrl, name, price, description) => {
-  return fetch(`http://localhost:3000/productos`, {
+  return fetch(`https://fake-api-rust.vercel.app/productos`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -31,7 +31,7 @@ const crearProducto = (imageUrl, name, price, description) => {
 
 // PUT/PATCH
 const alteraProducto = async (id, name, price, description) => {
-    return fetch(`http://localhost:3000/productos/${id}`, {
+    return fetch(`https://fake-api-rust.vercel.app/productos/${id}`, {
       method: "PATCH",
       headers: {
         "Content-Type": "application/json",
@@ -48,7 +48,7 @@ const alteraProducto = async (id, name, price, description) => {
 
   // DELETE
 const deleteProducto = async (id) => {
-    return await fetch(`http://localhost:3000/productos/${id}`, {
+    return await fetch(`https://fake-api-rust.vercel.app/productos/${id}`, {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
